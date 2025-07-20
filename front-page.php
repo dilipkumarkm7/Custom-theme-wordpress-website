0a631eeac88ab6d3ef5d6d0ac8b3433a64eecbc0
dilipkumarkm7 <dilip72765@gmail.com>
2025-07-20 12:02:56 +0530

    meta description included

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
 <head>
      <meta charset="<?php bloginfo('charset'); ?>">
      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="DK Theme - A custom-built, responsive WordPress theme with testimonials and optimized performance.">
      <?php wp_head(); ?>
 </head>
 
